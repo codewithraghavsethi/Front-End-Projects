1eb97f6906e681b155c01636fb4df355ad611575
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-23 10:36:22 +0530

    code

--- 16 Real Estate Website React - Tailwind CSS/real-estate/src/components/Navbar.jsx
+++ 16 Real Estate Website React - Tailwind CSS/real-estate/src/components/Navbar.jsx
@@ -4,8 +4,15 @@ import {assets} from "../assets/assets"
 const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full z-10 '>
-      <div>
-        <img src={assets} alt="" />
+      <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+        <img src={assets.logo} alt="" />
+        <ul>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+        </ul>
+        <button className='hidden md:block bg-white py-2 px-8 rounded-full'>Sign Up</button>
       </div>
     </div>
   )
