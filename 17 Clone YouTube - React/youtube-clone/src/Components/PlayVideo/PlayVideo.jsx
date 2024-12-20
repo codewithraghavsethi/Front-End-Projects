fbabc2c7e0916677db0b95f6fea68bbf7440a0c6
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-20 19:17:29 +0530

    code

---
+++ 17 Clone YouTube - React/youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./PlayVideo.css"
+import video1 from "../../assets/video1.mp4"
+import like from '../../assets/like.png'
+import deslike from '../../assets/deslike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+
+const PlayVideo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlayVideo
