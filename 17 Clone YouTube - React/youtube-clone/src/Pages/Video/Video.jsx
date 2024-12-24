2e7a6831b871304c961a2ece636c73948b7cfbff
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-24 07:28:24 +0530

    code

--- 17 Clone YouTube - React/youtube-clone/src/Pages/Video/Video.jsx
+++ 17 Clone YouTube - React/youtube-clone/src/Pages/Video/Video.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import "./Video.css"
-import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import React from "react";
+import "./Video.css";
+import PlayVideo from "../../Components/PlayVideo/PlayVideo";
+import Recommended from "../../Components/Recommended/Recommended";
 
 const Video = () => {
   return (
-    <div className='play-container'>
+    <div className="play-container">
       <PlayVideo />
+      <Recommended />
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
