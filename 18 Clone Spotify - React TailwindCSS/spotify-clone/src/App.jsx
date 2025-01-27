a2ddd9c3808a42b52e6cdf2c192efc51ea7d1072
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-01-27 17:50:52 +0530

    code

--- 18 Clone Spotify - React TailwindCSS/spotify-clone/src/App.jsx
+++ 18 Clone Spotify - React TailwindCSS/spotify-clone/src/App.jsx
@@ -1,11 +1,13 @@
-import Sidebar from './components/Sidebar'
+import Sidebar from "./components/Sidebar";
 
 function App() {
-   return (
-    <div className='hero'>
-      <Sidebar />
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- 18 Clone Spotify -/spotify-clone/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <div>
-        <h1 className="text-blue-500">hello Raghav</h1>
-    </div>
-  )
-}
-
-export default Navbar
