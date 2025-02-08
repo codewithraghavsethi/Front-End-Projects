9e291d77435ef63eeae1d42e1c3666546c5431f8
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-02-08 21:24:22 +0530

    code

--- 18 Clone Spotify - React TailwindCSS/spotify-clone/src/components/DisplayHome.jsx
+++ 18 Clone Spotify - React TailwindCSS/spotify-clone/src/components/DisplayHome.jsx
@@ -9,15 +9,13 @@ const DisplayHome = () => {
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
         <div className="flex overflow-auto">
-          {albumsData.map((item, index) => (
-            <AlbumItem
-              key={index}
-              name={item.name}
-              desc={item.desc}
-              id={item.id}
-              image={item.image}
-            />
-          ))}
+          {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} /> ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl"> Today,s begest hits</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} /> ))}
         </div>
       </div>
     </>
